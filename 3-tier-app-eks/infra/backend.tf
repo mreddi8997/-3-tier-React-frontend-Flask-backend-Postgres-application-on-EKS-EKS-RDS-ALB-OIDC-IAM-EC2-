60c0c269ca28d0d3e5af31e4df9a6450e5dcf184
Mredd <mreddi8997@gmail.com>
2026-08-06 18:31:16 -0230

    Add Terraform backend configuration for S3

---
+++ 3-tier-app-eks/infra/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-backend-mohit"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-2"
+    encrypt = true
+    use_lockfile = true
+  }
+}
