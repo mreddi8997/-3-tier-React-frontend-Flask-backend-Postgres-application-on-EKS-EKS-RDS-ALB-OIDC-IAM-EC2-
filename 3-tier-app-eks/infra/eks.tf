742f3f53b78e9cca2639ed700aa0e5b839c6fcdb
Mredd <mreddi8997@gmail.com>
2026-08-06 18:33:38 -0230

    Refactor EKS module configuration and settings
    Updated EKS module configuration to use the latest version and modified cluster name. Adjusted instance types for managed node groups and enabled public access to the cluster endpoint.

--- 3-tier-app-eks/infra/eks.tf
+++ 3-tier-app-eks/infra/eks.tf
@@ -1,47 +1,40 @@
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.33.1"
-
-  cluster_name    = "${var.prefix}-${var.environment}-cluster"
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 20.0" # Use the latest version of the EKS module
+  cluster_name    = "my-eks-cluster"
   cluster_version = "1.31"
+  subnet_ids      = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_3.id]
+  vpc_id          = aws_vpc.main.id
 
-  bootstrap_self_managed_addons = false
-  cluster_addons = {
-    coredns                = {}
-    eks-pod-identity-agent = {}
-    kube-proxy             = {}
-    vpc-cni                = {}
-  }
-
-  # Optional
+  # Enable public access to access cluster API via kubectl locally
   cluster_endpoint_public_access = true
 
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
-  vpc_id     = module.eks_network.vpc_id
-  subnet_ids = module.eks_network.private_subnets
+  cluster_additional_security_group_ids = [aws_security_group.vpc_all_traffic.id]
 
-  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    instance_types = ["t3.medium", "t3.micro"]
+    instance_types = ["t3.small", "t3.micro"]
+
+    depends_on = [aws_security_group.vpc_all_traffic, aws_vpc.main]
   }
 
+  # Fixed: Moved inside the module block and removed empty ami_type
   eks_managed_node_groups = {
     example = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["t3.medium"]
+      ami_type     = "AL2023_x86_64_STANDARD"  
+      instance_types = ["t3.small"]
 
       min_size     = 1
       max_size     = 3
       desired_size = 1
+
+      vpc_security_group_ids = [aws_security_group.vpc_all_traffic.id]
     }
   }
 
   tags = {
-    Environment = var.environment
+    Environment = "production"
     Terraform   = "true"
-    repo        = "DevOpsDojo"
   }
 }
