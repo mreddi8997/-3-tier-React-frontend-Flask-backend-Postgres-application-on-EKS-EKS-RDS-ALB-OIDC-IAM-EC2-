12bbfc4280bcb423702d7a297dc69922d0694888
Mredd <mreddi8997@gmail.com>
2026-08-25 10:37:20 -0230

    Correct typo in variable declaration for fetch
    Fix typo in fetch request variable declaration.

--- 3-tier-app-eks/frontend/src/components/Home.js
+++ 3-tier-app-eks/frontend/src/components/Home.js
@@ -11,7 +11,7 @@ function Home() {
     const fetchTopics = async () => {
       try {
         setLoading(true);
-        onst response = await fetch(`${API_URL}/api/topics`, {
+        const response = await fetch(`${API_URL}/api/topics`, {
           method: 'GET',
           headers: {
             'Accept': 'application/json',
