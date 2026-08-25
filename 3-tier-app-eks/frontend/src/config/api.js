db938ced8a30af5778354457a8ce964f36dd7c81
Mredd <mreddi8997@gmail.com>
2026-08-25 10:08:39 -0230

    Update API URL for production environment

--- 3-tier-app-eks/frontend/src/config/api.js
+++ 3-tier-app-eks/frontend/src/config/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL?.trim() || 'http://localhost:8000/api';
+const API_URL = process.env.REACT_APP_API_URL?.trim() || 'http://k8s-3tierapp-3tierapp-9415c65893-1241057560.us-east-2.elb.amazonaws.com';
 
 
 export default API_URL;
