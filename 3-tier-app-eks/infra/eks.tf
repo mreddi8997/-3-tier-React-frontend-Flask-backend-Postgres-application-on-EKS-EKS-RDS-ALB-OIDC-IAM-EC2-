9b9abd0a0d4ffce03a8395bbac7b41cab35d55fb
Mredd <mreddi8997@gmail.com>
2026-08-27 09:31:16 -0230

    Change instance type for EKS managed node group

--- 3-tier-app-eks/infra/eks.tf
+++ 3-tier-app-eks/infra/eks.tf
@@ -23,7 +23,7 @@ module "eks" {
   eks_managed_node_groups = {
     example = {
       ami_type     = "AL2023_x86_64_STANDARD"  
-      instance_types = ["c7i-flex.large"]
+      instance_types = ["t3.small"]
 
       min_size     = 2
       max_size     = 4
